03ba37863c3c90112efc79b7125fbfa56b3d84a3
ajikamaludin <aji.kamaludin@bankraya.co.id>
2023-03-15 11:46:59 +0700

    Initial Commit :tada:
    Author:    ajikamaludin <aji19kamaludin@gmail.com>
    
    fix template
    
    fix template
    
    fix template
    
    fix template

---
+++ resources/js/Components/Defaults/ApplicationLogo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from '@/Assets/logo.png';
+
+export default function ApplicationLogo({ className }) {
+    return (
+        <h1 className={className}>App Name</h1>
+    );
+}
