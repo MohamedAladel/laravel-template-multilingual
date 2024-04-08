07c5a0f5bfffc06a0b00da83157e42877e3562d9
Aji Kamaludin <aji19kamaludin@gmail.com>
2024-04-09 02:01:22 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,6 @@ use App\Http\Controllers\UploadController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return redirect('/login');
 });
@@ -51,4 +40,4 @@ Route::middleware(['auth'])->group(function () {
 });
 
 // #Guest
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
